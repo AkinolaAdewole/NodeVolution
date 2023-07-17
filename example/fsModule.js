550c563e37fd2fbe87d1c363e1b4f7c05383cb41
Akinola <adewoleakinola24@gmail.com>
2023-07-17 23:41:48 +0100

    NodeVolution

--- example/fsModule.js
+++ example/fsModule.js
@@ -5,12 +5,36 @@
 const fs=require("node:fs");
 
 //Synchronous Method
-const fileContent=fs.readFileSync("./Notes/FsModule.txt","utf-8");
+const fileContent=fs.readFileSync("./Notes/Test.txt","utf-8");
 console.log(fileContent);
 
 //CallBack Method
-fs.readFile("./Notes/FsModule.txt",(error, data)=>{
+fs.readFile("./Notes/Test.txt","utf-8",(error, data)=>{
     if(error){
         console.log(error);
+    }else{
+        console.log(data);
+    }
+});
+
+//To create and write a file
+// Synchronous Method
+fs.writeFileSync("./Notes/Test2.txt","Hello Adewole");
+
+//Asynchronous Method
+fs.writeFile('./Notes/Test2.txt',"  Adewole Stephen",{flag:"a"},(err)=>{
+    if(err){
+        console.log(err);
+    }else{
+        console.log("File Written");
+    }
+})
+
+
+fs.writeFile('./Notes/Test3',"Adewole Stephen",(err)=>{
+    if(err){
+        console.log(err);
+    }else{
+        console.log("File Written");
     }
 })
