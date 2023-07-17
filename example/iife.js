738b7d0113041924f4940f182ece2aa65898f951
Akinola <adewoleakinola24@gmail.com>
2023-07-17 15:41:36 +0100

    Nodevolution

---
+++ example/iife.js
@@ -0,0 +1,12 @@
+(function(Message){
+    const superHero='Batman';
+    console.log(Message,superHero);
+})('Hello');
+
+(function (Message){
+    const superHero='Superman';
+    console.log(Message,superHero);
+})('Hey');
+
+
+
