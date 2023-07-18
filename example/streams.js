77fd39cc18a711d0b660f12a92a65a41eda0ab72
Akinola <adewoleakinola24@gmail.com>
2023-07-18 10:05:20 +0100

    NodeVolution

--- example/streams.js
+++ example/streams.js
@@ -1,3 +1,9 @@
 const fs =require("node:fs");
 
-const readableStreams=fs.createReadStream()
+
+// This method accept file path as the first argument and option object as the second argument
+const readableStreams=fs.createReadStream('./Notes/Test.txt',{
+    encoding:"utf-8"
+});
+
+const writeableStreams=fs.createWriteStream('./Notes/Test2.txt');
