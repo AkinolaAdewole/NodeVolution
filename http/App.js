d53d1c4eaa1a287fce47630c1d6b944cf6cd274d
Akinola <adewoleakinola24@gmail.com>
2023-07-18 11:52:17 +0100

    Node Server

--- http/App.js
+++ http/App.js
@@ -1,6 +1,12 @@
 const http = require("http");
 
 const server=http.createServer((req,res)=>{
+
+    const superHero={
+        firstName:'Akinola',
+        lastName:'Adewole'
+    }
+
     res.writeHead(200,{"Content-Type":"text/plain"});
     res.end('Hello World');
 })
