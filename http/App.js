6be9ea041ae231e8cffd8a1b13c3d03427c786c8
Akinola <adewoleakinola24@gmail.com>
2023-07-18 12:05:08 +0100

    Node Server

--- http/App.js
+++ http/App.js
@@ -8,7 +8,7 @@ const server=http.createServer((req,res)=>{
     }
 
     res.writeHead(200,{"Content-Type":"text/plain"});
-    res.end('Hello World');
+    res.end(superHero);
 })
 
 server.listen(3000,()=>{
